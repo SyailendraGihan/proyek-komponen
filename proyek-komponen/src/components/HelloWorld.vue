98f9246b01831ae083e9959fc64333f261a555de
Syailendra Gihan <147775314+SyailendraGihan@users.noreply.github.com>
2025-03-31 16:30:43 +0000

    Menambahkan folder public dan src

---
+++ proyek-komponen/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <HelloWorld msg="Halo, ini komponen HelloWorld!"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld
+  }
+};
+</script>
---
+++ proyek-komponen/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Selamat Datang di HomeView</h1>
+    <p>Komponen ini sekarang tidak menggunakan HelloWorld.vue</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView"
+};
+</script>
